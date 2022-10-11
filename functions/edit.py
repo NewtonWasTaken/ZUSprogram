40cd42525506e31d8e46de6805ebfe102544b7ed
BestGoofy <70072322+BestGoofy@users.noreply.github.com>
2022-10-11 20:57:37 +0200

    added edit.py with class Tables(), a lots of comments added

---
+++ functions/edit.py
@@ -0,0 +1,63 @@
+class Tables():
+    def __init__(self, table_1, table_2):
+        self.table_1 = table_1
+        self.table_2 = table_2
+
+
+    # Merging some table_1 informations to table_2
+    def merge(self):
+        #Handeling more Skladba or Žáci
+        for i in range(len(self.table_1["Autor"])):
+            if self.table_1["Č."][i] == 0.0 and self.table_1["Autor"] != 0:
+                self.table_1["Autor"][i-1] = self.table_1["Autor"][i-1] + " \n" + self.table_1["Autor"][i]
+                self.table_1["Skladba"][i - 1] = self.table_1["Skladba"][i - 1] + " \n" + self.table_1["Skladba"][i]
+                self.table_1["Autor"][i] = 0
+                self.table_1["Skladba"][i] = 0
+        #Deleting empty strings
+        for i in range(len(self.table_1["Autor"])):
+            try:
+                if self.table_1["Autor"][i] == 0:
+                    del self.table_1["Autor"][i]
+                    del self.table_1["Skladba"][i]
+                    del self.table_1["Č."][i]
+
+            except:
+                i = i - 1
+                if self.table_1["Autor"][i] == 0:
+                    del self.table_1["Autor"][i]
+                    del self.table_1["Skladba"][i]
+                    del self.table_1["Č."][i]
+
+        del self.table_2["Skladby"]
+        #Merging itself
+        self.table_2.update({"Skladba": []})
+        self.table_2.update({"Autor": []})
+        self.table_2["Skladba"] += self.table_1["Skladba"]
+        self.table_2["Autor"] += self.table_1["Autor"]
+        return(self.table_2)
+
+    def sort(self):
+        # Deletes unused lists
+        del self.table_2["Poznámka"]
+        del self.table_2["Učitel"]
+        del self.table_2["Délka"]
+        # Orders table corectly
+        order = ("Číslo", "Autor", "Skladba", "Žák (žáci)", "Ročník", "Předmět")
+        final_table = {}
+        for i in order:
+            final_table[i] = self.table_2[i]
+        return (final_table)
+
+    def format(self, type="korepetice"):
+        #formats number and korepetice
+        if type == "korepetice":
+            for i in range(len(self.table_2["Poznámka"])):
+                if self.table_2["Poznámka"][i] != 0.0:
+                    self.table_2["Předmět"][i] = self.table_2["Předmět"][i] + " \n" + self.table_2["Poznámka"][i]
+                self.table_2["Číslo"][i] = f'{self.table_2["Číslo"][i]}.'
+        #formats only number
+        elif type == "numbers":
+            for i in range(len(self.table_2["Číslo"])):
+                self.table_2["Číslo"][i] = f'{self.table_2["Číslo"][i]}.'
+
+
--- main.py
+++ main.py
@@ -1,45 +1,58 @@
 import pandas
 import pandas as pd
+from functions.edit import Tables
 
+
+#Imports tables and converts them to dict.
+#Nan values are changed to 0
 table_1_list = pd.read_html("program1.xls")
 table_1 = table_1_list[0].fillna(0).to_dict(orient="list")
 table_2_list = pd.read_html("program2.xls")
 table_2 = table_2_list[0].fillna(0).to_dict(orient="list")
 
 
-for i in range(len(table_1["Autor"])):
-    if table_1["Autor"][i] == 0:
-        del table_1["Autor"][i]
+#Creates Tables object
+tb = Tables(table_1, table_2)
 
-for i in range(len(table_1["Skladba"])):
-    if table_1["Skladba"][i] == 0:
-        del table_1["Skladba"][i]
-del table_2["Skladby"]
-table_2.update({"Skladba":[]})
-table_2.update({"Autor":[]})
-table_2["Skladba"] += table_1["Skladba"]
-table_2["Autor"] += table_1["Autor"]
+#Merges two tables into one
+tb.merge()
 
+#Formats Poznámka and Číslo in table
+tb.format(type="korepetice")
+
+
+
+#Merging Poznámka cells to Předmět cells and editing numbering system
 for i in range(len(table_2["Poznámka"])):
     if table_2["Poznámka"][i] != 0.0:
         table_2["Předmět"][i] = table_2["Předmět"][i] + " \n" + table_2["Poznámka"][i]
-
-
-for i in range(len(table_2["Číslo"])):
     table_2["Číslo"][i] = f'{table_2["Číslo"][i]}.'
 
 
-del table_2["Poznámka"]
-del table_2["Učitel"]
-del table_2["Délka"]
 
-order = ("Číslo", "Autor", "Skladba", "Žák (žáci)", "Ročník", "Předmět")
-final_table = {}
-for i in order:
-    final_table[i] = table_2[i]
-table_dataframe = pandas.DataFrame(final_table)
-table_dataframe.to_excel("program_result.xlsx", index=False)
-print(table_1)
+
+
+#Exports data to final table
+table_dataframe = pandas.DataFrame(tb.sort())
+
+
+#Exports to excel table and opens a writer
+writer = pd.ExcelWriter('program_result.xlsx', engine='xlsxwriter')
+table_dataframe.to_excel(writer, index=False)
+workbook  = writer.book
+worksheet = writer.sheets['Sheet1']
+
+#Cusomizes columns width and warps text \n
+text_wrap_format = workbook.add_format({'text_wrap': True})
+worksheet.set_column(0, 0, 5, text_wrap_format)
+worksheet.set_column(1, 1, 30, text_wrap_format)
+worksheet.set_column(2, 3, 45, text_wrap_format)
+worksheet.set_column(4, 4, 7, text_wrap_format)
+worksheet.set_column(5, 5, 30, text_wrap_format)
+writer.close()
+
+#Printing variables
+print(tb.table_2)
 print(table_2)
-print(table_dataframe)
+print(table_1)
 
